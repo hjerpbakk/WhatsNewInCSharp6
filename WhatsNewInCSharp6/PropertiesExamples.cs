51fe6fa53f9b9fde39d63f5f05e5a9a489467fa7
Runar Ovesen Hjerpbakk <hjerpbakk@gmail.com>
2016-01-06 16:49:45 +0100

    Added properties examples

---
+++ WhatsNewInCSharp6/PropertiesExamples.cs
@@ -0,0 +1,57 @@
+﻿namespace WhatsNewInCSharp6
+{
+    public class OldDefaultValues
+    {
+        // Too much code 
+        public OldDefaultValues()
+        {
+            PropertyWithDefaultValue = "Default value";
+        }
+
+        public string PropertyWithDefaultValue { get; set; }
+    }
+
+    /// <summary>
+    /// Initializers for auto-properties
+    /// </summary>
+    public class NewDefaultValues
+    {
+        public string PropertyWithDefaultValue { get; set; } = "Default value";
+    }
+
+    public class OldImmutableClass
+    {
+        // Redundant backing field
+        private readonly string readOnlyBackingField;
+
+        public OldImmutableClass(string theValue)
+        {
+            readOnlyBackingField = theValue;
+            PrivateAutoProperty = "Default value";
+        }
+
+        // Too much code again
+        public string ReadOnlyProperty { get { return readOnlyBackingField;  } }
+
+        // Not really immutable
+        public string PrivateAutoProperty { get; private set; }
+    }
+
+    /// <summary>
+    /// Getter-only auto-properties
+    /// </summary>
+    public class ProperImmutableClass
+    {
+        public ProperImmutableClass(string theValue)
+        {
+            ReadOnlyProperty = theValue;
+        }
+
+        // Readonly backing field added by the compiler
+        public string ReadOnlyProperty { get; }
+
+        // Readonly properties can also have default values,
+        // but this value can still be overridden in a constructor
+        public string ReadOnlyPropertyWithDefaultValue { get; } = "Default value";
+    }
+}
