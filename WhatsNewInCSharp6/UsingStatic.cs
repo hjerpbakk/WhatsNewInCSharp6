9b2294ece5c8d14c057079b9eaf85da4b98933f6
Runar Ovesen Hjerpbakk <hjerpbakk@gmail.com>
2016-01-12 22:08:10 +0100

    Updated using static

--- WhatsNewInCSharp6/UsingStatic.cs
+++ WhatsNewInCSharp6/UsingStatic.cs
@@ -1,5 +1,9 @@
-using static System.Math;
+// Super awesome static using
+using static System.Math; 
 using static System.DayOfWeek;
+
+// The type, not the namespace
+using static System.Linq.Enumerable; 
 
 namespace WhatsNewInCSharp6
 {
@@ -19,7 +23,16 @@ namespace WhatsNewInCSharp6
                 return;    
             }
             
+            // Such readability
             return (Pi * e) / 0,2033270053; 
+        }
+        
+        public int[] EvenNumbers(int lowerLimit, int upperLimit)
+        {
+            var range = Range(lowerLimit, upperLimit);
+            
+            // Even without using System.Linq 
+            return range.Where(i => i % 2 == 0).ToArray();
         }
     }    
 }
